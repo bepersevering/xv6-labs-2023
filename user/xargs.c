79858746188e65f888fe020afd29e7d8cb57e5f2
bepersevering <mingshashan@gmail.com>
2024-03-26 20:17:18 +0800

    learn->xargs

--- user/xargs.c
+++ user/xargs.c
@@ -1,4 +1,48 @@
+
+#include "kernel/param.h"
+#include "kernel/stat.h"
 #include "kernel/types.h"
 #include "user/user.h"
 
-int main(int argc, char *argv[]) {}
+#define MAXN 1024
+// xargs, MAXARG
+int main(int argc, char *argv[]) {
+  if (argc < 2) {
+    fprintf(2, "Usage: xargs <command> \n");
+    exit(1);
+  }
+
+  char *xargv[MAXARG];
+  char buf[MAXN];
+  char temp[MAXN];
+  int index;
+  int xi;
+
+  // copy command的参数
+  for (int = 1; i < argc; i++) {
+    xargv[xi++] = argv[i];
+  }
+
+  // 从标准输入获取
+  while (read(1, buf, MAXN) > 0) {
+    buf[MAXN] = '\0';
+    index = 0;
+
+    for (int i = 0; i < strlen(buf); i++) {
+      if (buf[i] == '\n') {
+        // 如果遇到换行符，则直接执行
+        temp[index] = '\0';
+        xargv[xi] = temp;
+        index = 0;
+        if (fork() == 0) {
+          // child process
+          exec(argv[1], xargv);
+        }
+        wait(0);
+      } else {
+        temp[index++] = buf[i];
+      }
+    }
+  }
+  exit(0);
+}
